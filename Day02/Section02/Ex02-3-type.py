c2dcac1fcf336b599cd142be153ad2803d59fc0e
ITSC <onnhee@naver.com>
2022-11-20 13:13:34 +0900

    day02 수업

--- Day02/Section02/Ex02-3-type.py
+++ Day02/Section02/Ex02-3-type.py
@@ -1,16 +1,16 @@
 '''
-[내장 데이터 유형]
-    Python 변수는 다른 유형의 데이터를 저장할 수 있으며
-    다른 유형은 다른 작업을 수행할 수 있다.
+    [내장 데이터 유형]
+        Python 변수는 다른 유형의 데이터를 저장할 수 있으며
+        다른 유형은 다른 작업을 수행할 수 있다.
 
-    텍스트 유형 : str
-    숫자 유형 : int, float, complex
-    시퀀스 유형 : list, tuple, rage
-    매핑 유형 : dict
-    세트 유형 : set
-    부울(논리) 유형 : bool
-    바이너리 유형 : bytes, bytearray
-    없음 유형 : NoneType
+        텍스트 유형 : str
+        숫자 유형 : int, float, complex
+        시퀀스 유형 : list, tuple, rage
+        매핑 유형 : dict
+        세트 유형 : set
+        부울(논리) 유형 : bool
+        바이너리 유형 : bytes, bytearray
+        없음 유형 : NoneType
 '''
 # str
 x = "Hello World"
--- Day02/Section02/Ex02-6-tuple.py
+++ Day02/Section02/Ex02-6-tuple.py
@@ -1,12 +1,15 @@
 ''''
     tuple:
-
+        단일 변수에 여러 항목을 저장하는데 사용된다.
+        순서가 있고, 변경할 수 없는 List
+        둥근 괄호로 작성된다. ()
 '''
 thistuple = ("피카츄", "라이츄", "파이리")
 print(thistuple)
-# 튜플 길이 변경
-print(thistuple[1:3])
-print(thistuple[1:3])
+# 튜플 길이 len()
+print(len(thistuple))
+print(thistuple[1])
+print(thistuple[-1])
 print(thistuple[1:3])
 
 # 튜플값 변경방법
@@ -16,13 +19,13 @@ thiscast[1] = "잠만보"
 thistuple = tuple(thiscast)
 print(thistuple)
 
-#튜플 압축풀기
-thistuple = ("피카츄", "라이츄", "꼬부기")
+# 튜플 압축풀기
+thistuple = ("피카츄", "라이츄", "파이리", "꼬부기")
 (p1, p2, p3, p4) = thistuple
-print(type(1))
-print(2)
-print(3)
-print(4)
+print(type(p1))
+print(p2)
+print(p3)
+print(p4)
 
 # 두개 튜플 조인
 thistuple1 = ("피카츄", "라이츄", "파이리", "꼬부기")
