062bc0853ffd2dc0443dd448ab0b23a134e434fe
ITSC <onnhee@naver.com>
2022-12-05 13:56:32 +0900

    day04 수업

--- Day04/Section06/Ex06-3-while3.py
+++ Day04/Section06/Ex06-3-while3.py
@@ -1,7 +1,7 @@
 my_list = []
 n = 1 #초기화
 while n != 0:
-    n = int(input('정수를 입력하세요(종료는 0입니다.)>>>'))
+    n = int(input('정수를 입력하세요(종료는 0입니다) >>>'))
     my_list.append(n)
 my_list.pop()   # 0은 필요없으니까
 print(my_list)
