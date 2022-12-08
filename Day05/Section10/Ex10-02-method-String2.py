b0fc4ad31a7049ec86176f4a74250b0b76512f9c
ITSC <onnhee@naver.com>
2022-12-08 15:44:13 +0900

    3주차

--- Day05/Section10/Ex10-02-method-String2.py
+++ Day05/Section10/Ex10-02-method-String2.py
@@ -50,10 +50,18 @@ print(start_end)  # 앞뒤제거
 all = s.replace(' ', '')
 print(all)  # 모두제거
 
+# 교집합 intersection()
+s1 = {'apple', 'banana', 'cherry'}
+s2 = {'apple', 'banana', 'orange'}
+print(s1 & s2)
+
+result = s1.intersection(s2)
+print(result)
+
 # 합집합
 s1 = {'banana', 'apple', 'cherry'}
 s2 = {'banana', 'apple', 'orange'}
-print(s1 + s2)
+print(s1 | s2)
 
 plus = s1.union(s2)
 print(plus)
